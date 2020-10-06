09a963d23d9a961187963722bf11c33c660ae751
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-10-06 15:59:36 +0200

    Reactive ExploreDetails

--- src/frontend/components/ExploreSection/ExploreOptions/ExploreDetails/ExploreDetails.js
+++ src/frontend/components/ExploreSection/ExploreOptions/ExploreDetails/ExploreDetails.js
@@ -6,13 +6,21 @@ const ExploreDetails = props => (
   <div id='ExploreDetails' style={{order: props.selectedCategory}}>
     {(props.categories.length > 0) &&
       props.categories[props.selectedCategory].activities.map((activity, index) => (
-        <div key={'activity'+index}>
-          <p onClick={() => props.selectActivity(index)}>{activity.name}</p>
-          {(props.selectedActivity === index) &&
-            <p>{activity.description}</p>
-          }
-        </div>
+        <p
+          className={'activityName'}
+          key={'activity'+index}
+          style={{order: index}}
+          onClick={() => props.selectActivity(index)}
+        >
+          {activity.name}
+        </p>
       ))
+    }
+
+    {(props.selectedActivity !== -1) &&
+      <p className='activityDescription' style={{order: props.selectedActivity}}>
+        {props.categories[props.selectedCategory].activities[props.selectedActivity].description}
+      </p>
     }
   </div>
 );
