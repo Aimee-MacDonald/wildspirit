007a4616a9f7bee2faeb208efecaa39108f224cc
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-10-14 18:43:55 +0200

    WeatherWidget fully implemented

--- src/frontend/components/HomeSection/WeatherWidget/WeatherWidget.js
+++ src/frontend/components/HomeSection/WeatherWidget/WeatherWidget.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
-const WeatherWidget = () => (
-  <div>
-    <p>17C</p>
-    <p>CLOUDY</p>
-  </div>
-);
+import './WeatherWidget.sass';
 
-export default WeatherWidget;
+export default class WeatherWidget extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      description: "",
+      temperature: 0
+    }
+  }
+
+  render(){
+    return(
+      <div id="WeatherWidget">
+        {!!this.state.temperature && <p>{this.state.temperature}<span>&#176;</span>C</p>}
+        <p>{this.state.description}</p>
+      </div>
+    );
+  }
+
+  componentDidMount(){
+    window.fetch('/api/weather')
+      .then(res => res.json())
+      .then(result => {
+        this.setState(() => (result))
+      })
+  }
+}
