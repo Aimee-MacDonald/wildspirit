fcb99142c691a7999085703b62e17b8485ffa49a
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-10-19 16:16:35 +0200

    LearnOptions data now live on the backend

--- src/backend/routes/api.js
+++ src/backend/routes/api.js
@@ -57,4 +57,28 @@ router.get('/accommodation', (req, res) => {
   res.status(200).json(respac);
 });
 
+router.get('/events', (req, res) => {
+  const respac = [{
+    "imgURL": "https://res.cloudinary.com/dcmdpotqs/image/upload/v1602604754/LearnOptions/IMG-20200808-WA0026_jokxd5.jpg",
+    "imgAlt": "A happy group of friends planting trees together",
+    "title": "Reforestation",
+    "subtitle": "Recreating the indigenous forest",
+    "description": "As part of the Greenpop initiative, we are planting over 300 trees in an area of cleared alien vegetation. Bring all your friends and a picnic basket and come join us in making a real and lasting difference to our natural environment."
+  }, {
+    "imgURL": "https://res.cloudinary.com/dcmdpotqs/image/upload/v1602604745/LearnOptions/Bush_camp_sign_20_mdesil.jpg",
+    "imgAlt": "A Gardener and his tools meditating with a horse",
+    "title": "Concious Gardening",
+    "subtitle": "Build the perfect garden and a mind to match it",
+    "description": "A healthy mind promotes a healthy environment. A healthy environment promotes a healthy mind. Come join us and start your own journey to a happier, healthier you and a beautiful garden to enjoy it in."
+  }, {
+    "imgURL": "https://res.cloudinary.com/dcmdpotqs/image/upload/v1602604744/LearnOptions/Bush_camp_sign_18_jkco8r.jpg",
+    "imgAlt": "A group of people watching a band playing live outside.",
+    "title": "Music on the Deck",
+    "subtitle": "Live music at Wild Spirit",
+    "description": "Join us for an unforgettable night of live music and festivities at wild spirit."
+  }];
+
+  res.status(200).json(respac);
+});
+
 module.exports = router;
--- src/frontend/components/LearnSection/LearnOptions/LearnOptions.js
+++ src/frontend/components/LearnSection/LearnOptions/LearnOptions.js
@@ -4,26 +4,6 @@ import './LearnOptions.sass';
 
 import LearnOption from './LearnOption/LearnOption';
 
-const learnOptionsData = [{
-  "imgURL": "https://res.cloudinary.com/dcmdpotqs/image/upload/v1602604754/LearnOptions/IMG-20200808-WA0026_jokxd5.jpg",
-  "imgAlt": "A happy group of friends planting trees together",
-  "title": "Reforestation",
-  "subtitle": "Recreating the indigenous forest",
-  "description": "As part of the Greenpop initiative, we are planting over 300 trees in an area of cleared alien vegetation. Bring all your friends and a picnic basket and come join us in making a real and lasting difference to our natural environment."
-}, {
-  "imgURL": "https://res.cloudinary.com/dcmdpotqs/image/upload/v1602604745/LearnOptions/Bush_camp_sign_20_mdesil.jpg",
-  "imgAlt": "A Gardener and his tools meditating with a horse",
-  "title": "Concious Gardening",
-  "subtitle": "Build the perfect garden and a mind to match it",
-  "description": "A healthy mind promotes a healthy environment. A healthy environment promotes a healthy mind. Come join us and start your own journey to a happier, healthier you and a beautiful garden to enjoy it in."
-}, {
-  "imgURL": "https://res.cloudinary.com/dcmdpotqs/image/upload/v1602604744/LearnOptions/Bush_camp_sign_18_jkco8r.jpg",
-  "imgAlt": "A group of people watching a band playing live outside.",
-  "title": "Music on the Deck",
-  "subtitle": "Live music at Wild Spirit",
-  "description": "Join us for an unforgettable night of live music and festivities at wild spirit."
-}];
-
 export default class LearnOptions extends React.Component{
   constructor(props){
     super(props);
@@ -54,20 +34,24 @@ export default class LearnOptions extends React.Component{
   }
 
   componentDidMount(){
-    let options = [];
+    fetch('/api/events')
+      .then(res => res.json())
+      .then(result => {
+        let options = [];
+        
+        result.forEach(option => {
+          options.push({
+            imgURL: option.imgURL,
+            imgAlt: option.imgAlt,
+            title: option.title,
+            subtitle: option.subtitle,
+            description: option.description,
+            enquiryOpen: false
+          });
+        });
 
-    learnOptionsData.forEach(option => {
-      options.push({
-        imgURL: option.imgURL,
-        imgAlt: option.imgAlt,
-        title: option.title,
-        subtitle: option.subtitle,
-        description: option.description,
-        enquiryOpen: false
+        this.setState(() => ({options: options}));
       });
-    });
-
-    this.setState(() => ({options: options}));
   }
 
   showEnquiry(optionIndex){
