cfbf4c6e2b67ba7e2e8ad3b48a44cd26f1c12ddb
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-10-19 13:13:56 +0200

    LearnOptionInfo styling

--- src/frontend/components/LearnSection/LearnOptions/LearnOption/LearnOptionInfo/LearnOptionInfo.js
+++ src/frontend/components/LearnSection/LearnOptions/LearnOption/LearnOptionInfo/LearnOptionInfo.js
@@ -4,11 +4,17 @@ import './LearnOptionInfo.sass';
 
 const LearnOptionInfo = props => (
   <div className='LearnOptionInfo'>
-    <p>{props.title}</p>
-    <p>{props.subtitle}</p>
-    <p>{props.description}</p>
-    <p>Share Links</p>
-    <button onClick={props.showEnquiry}>Book</button>
+    <div className='learnInfoTitle'>
+      <h3>{props.title}</h3>
+      <p>{props.subtitle}</p>
+    </div>
+
+    <p className='learnInfoDescription'>{props.description}</p>
+
+    <div className='learnInfoControls'>
+      <button onClick={props.showEnquiry}>Book</button>
+      <p>Share Links</p>
+    </div>
   </div>
 );
 
