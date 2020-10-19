3fb2d426bc89fee1c2788706202fea6999c53d04
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-10-19 16:22:54 +0200

    Moved ExploreOptions data to the backend

--- src/frontend/components/ExploreSection/ExploreOptions/ExploreOptions.js
+++ src/frontend/components/ExploreSection/ExploreOptions/ExploreOptions.js
@@ -4,77 +4,6 @@ import './ExploreOptions.sass';
 
 import ExploreDetails from './ExploreDetails/ExploreDetails';
 
-const exploreData = [{
-  "name": "Hiking Trails",
-  "activities": [{
-    "name": "Harkerville Forest",
-    "description": "activity description"
-  }, {
-    "name": "Salt River Hike",
-    "description": "activity description"
-  }, {
-    "name": "Kalanderkloof Hike",
-    "description": "activity description"
-  }, {
-    "name": "Otter Trail",
-    "description": "activity description"
-  }, {
-    "name": "Robberg Nature Reserve",
-    "description": "activity description"
-  }]
-}, {
-  "name": "Beaches",
-  "activities": [{
-    "name": "Nature's Valley Beach",
-    "description": "activity description"
-  }, {
-    "name": "Keurbooms Beach",
-    "description": "activity description"
-  }, {
-    "name": "Central Beach",
-    "description": "activity description"
-  }, {
-    "name": "Robberg Beach",
-    "description": "activity description"
-  }]
-}, {
-  "name": "Water Activities",
-  "activities": [{
-    "name": "Sea Kayaking Robberg",
-    "description": "activity description"
-  }, {
-    "name": "Keurbooms River Canooing",
-    "description": "activity description"
-  }, {
-    "name": "Groot River Lagoon Canooing",
-    "description": "activity description"
-  }]
-}, {
-  "name": "Animal Activities",
-  "activities": [{
-    "name": "Monkey Land",
-    "description": "activity description"
-  }, {
-    "name": "Birds of Eden",
-    "description": "activity description"
-  }, {
-    "name": "Tenikwa Wildlife Rehabilitation",
-    "description": "activity description"
-  }, {
-    "name": "Elephant Sanctuary",
-    "description": "activity description"
-  }]
-}, {
-  "name": "Adrenaline Activities",
-  "activities": [{
-    "name": "Skydive Plett",
-    "description": "activity description"
-  }, {
-    "name": "Bloukrans Bungy Jump",
-    "description": "activity description"
-  }]
-}];
-
 export default class ExploreOptions extends React.Component{
   constructor(props){
     super(props);
@@ -111,7 +40,11 @@ export default class ExploreOptions extends React.Component{
   }
 
   componentDidMount(){
-    this.setState(() => ({categories: exploreData}));
+    fetch('/api/activities')
+      .then(res => res.json())
+      .then(result => {
+        this.setState(() => ({categories: result}));
+      })
   }
 
   selectCategory(categoryIndex){
