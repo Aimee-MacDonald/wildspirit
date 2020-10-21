5d0698d7d1105700e1b56771de06122339319bca
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-10-21 11:30:01 +0200

    HomeSection Nav linkup and styling

--- src/frontend/components/HomeSection/Navigation/Navigation.js
+++ src/frontend/components/HomeSection/Navigation/Navigation.js
@@ -5,11 +5,12 @@ import './Navigation.sass';
 const Navigation = () => (
   <nav id='Navigation'>
     <ul>
-      <li><a href='#'>Live</a></li>
-      <li><a href='#'>Learn</a></li>
-      <li><a href='#'>Explore</a></li>
+      <li><a href='#LiveSection'>Live</a></li>
+      <li><a href='#LearnSection'>Learn</a></li>
+      <li><a href='#ExploreSection'>Explore</a></li>
     </ul>
   </nav>
 );
+
 
 export default Navigation;
