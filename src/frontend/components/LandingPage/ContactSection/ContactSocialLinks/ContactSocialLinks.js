a76fdcb41683a0059098c74a66480d37acaae9a3
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-10-31 11:40:46 +0200

    Social links in contact section

--- src/frontend/components/LandingPage/ContactSection/ContactSocialLinks/ContactSocialLinks.js
+++ src/frontend/components/LandingPage/ContactSection/ContactSocialLinks/ContactSocialLinks.js
@@ -4,9 +4,9 @@ import './ContactSocialLinks.sass';
 
 const ContactSocialLinks = () => (
   <ul id='ContactSocialLinks'>
-    <li><a href='#'>Instagram</a></li>
-    <li><a href='#'>Twitter</a></li>
-    <li><a href='#'>Facebook</a></li>
+    <li><a href='#' id='instagram'><span className="hiddenLabel">Instagram</span></a></li>
+    <li><a href='#' id='twitter'><span className="hiddenLabel">Twitter</span></a></li>
+    <li><a href='#' id='facebook'><span className="hiddenLabel">Facebook</span></a></li>
   </ul>
 );
 
