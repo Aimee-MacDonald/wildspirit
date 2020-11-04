311e51487ac056898d94b686daf431e9df2c0684
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-11-04 15:59:10 +0200

    Backend Rendering

--- src/backend/server.js
+++ src/backend/server.js
@@ -9,12 +9,19 @@ const api = require(path.join(__dirname, "/routes/api"));
 
 mongoose.connect(process.env.DBURL, {useNewUrlParser: true, useUnifiedTopology: true});
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "/views"));
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../../dist')));
 app.use("/api", api);
 
+app.get('/', (req, res) => {
+  res.status(200).render("index");
+});
+
 app.get("/admin", (req, res) => {
-  res.redirect("/admin.html");
+  res.status(200).render('admin');
 });
 
 app.listen(process.env.PORT, () => console.log(`Server Listening on Port: ${process.env.PORT}`));
