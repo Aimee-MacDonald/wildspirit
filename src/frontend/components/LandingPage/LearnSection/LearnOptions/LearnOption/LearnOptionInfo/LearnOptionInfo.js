5bd0ff92412469c4ad265d29d353d965d8c85294
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-01-21 16:10:03 +0200

    LearnOption share button

--- src/frontend/components/LandingPage/LearnSection/LearnOptions/LearnOption/LearnOptionInfo/LearnOptionInfo.js
+++ src/frontend/components/LandingPage/LearnSection/LearnOptions/LearnOption/LearnOptionInfo/LearnOptionInfo.js
@@ -13,7 +13,7 @@ const LearnOptionInfo = props => (
 
     <div className='learnInfoControls'>
       <button onClick={props.showEnquiry}>Book</button>
-      <p>Share Links</p>
+      <div class="fb-share-button" data-href="https://wildspiritlodge.herokuapp.com/#LearnSection" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwildspiritlodge.herokuapp.com%2F%23LearnSection&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
     </div>
   </div>
 );
