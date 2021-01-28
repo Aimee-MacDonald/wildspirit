22da4d1a53ea527f3e5294d566b37a90252e1a4d
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-01-28 12:23:52 +0200

    ContactSection micro-interactions

--- src/frontend/components/LandingPage/ContactSection/ContactMessageBox/ContactMessageBox.js
+++ src/frontend/components/LandingPage/ContactSection/ContactMessageBox/ContactMessageBox.js
@@ -5,7 +5,7 @@ import './ContactMessageBox.sass';
 const ContactMessageBox = () => (
   <div id='ContactMessageBox'>
     <label htmlFor='contactMessage'>Message</label>
-    <textarea id='contactMessage' name='CIMessage' placeholder='Message' />
+    <textarea id='contactMessage' name='CIMessage' placeholder='Message' required />
   </div>
 );
 
--- src/frontend/components/LandingPage/ContactSection/ContactSection.js
+++ src/frontend/components/LandingPage/ContactSection/ContactSection.js
@@ -11,26 +11,66 @@ import ContactSocialLinks from './ContactSocialLinks/ContactSocialLinks';
 export default class ContactSection extends React.Component{
   constructor(props){
     super(props);
+
+    this.state = {
+      formActive: true,
+      messageLoading: false,
+      messageSucceed: false,
+      messageFail: false
+    }
+
+    this.sendMessage = this.sendMessage.bind(this);
   }
 
   render(){
     return(
-      <form id='ContactSection' onSubmit={this.sendMessage}>
+      <div id='ContactSection' onSubmit={this.sendMessage}>
         <ContactImage />
-        <div id='contactContent'>
-          <ContactMessageBox />
-          <div id='contactSmallContent'>
-            <ContactInputs />
-            <ContactSendButton />
-            <ContactSocialLinks />
+
+        {this.state.formActive &&
+          <form id='contactContent'>
+            <ContactMessageBox />
+            <div id='contactSmallContent'>
+              <ContactInputs />
+              <ContactSendButton />
+              <ContactSocialLinks />
+            </div>
+          </form>
+        }
+
+        {this.state.messageLoading &&
+          <div className='messageLoading'>
+            <div className='spinner'><div></div></div>
+            <h1>Loading</h1>
           </div>
-        </div>
-      </form>
+        }
+
+        {this.state.messageSucceed &&
+          <div className='messageSucceed'>
+            <h1>Success</h1>
+            <p>Thank you for your enquiry, we will respond to you as soon as possible.</p>
+          </div>
+        }
+
+        {this.state.messageFail &&
+          <div className='messageFail'>
+            <h1>Fail</h1>
+            <p>An error occured, please check your connection and try again.</p>
+          </div>
+        }
+      </div>
     )
   }
 
   sendMessage(event){
     event.preventDefault();
+
+    this.setState(() => ({
+      formActive: false,
+      messageLoading: true,
+      messageSucceed: false,
+      messageFail: false
+    }));
 
     const formData = {
       'name': event.target.CIName.value,
@@ -44,7 +84,30 @@ export default class ContactSection extends React.Component{
       body: JSON.stringify({'message': formData})
     })
     .then(response => response.json())
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
+    .then(result => {
+      if(result === 'Success'){
+        this.setState(() => ({
+          formActive: false,
+          messageLoading: false,
+          messageSucceed: true,
+          messageFail: false
+        }));
+      } else {
+        this.setState(() => ({
+          formActive: false,
+          messageLoading: false,
+          messageSucceed: false,
+          messageFail: true
+        }));
+      }
+    })
+    .catch(error => {
+      this.setState(() => ({
+        formActive: false,
+          messageLoading: false,
+          messageSucceed: false,
+          messageFail: true
+      }));
+    })
   }
 }
