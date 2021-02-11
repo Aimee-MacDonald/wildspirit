b04c10d48e0d2484d6307863a0b254d4e18d174c
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-02-11 15:59:55 +0200

    ExploreDetails basic layout

--- src/frontend/components/LandingPage/ExploreSection/ExploreDetails/ExploreDetails.js
+++ src/frontend/components/LandingPage/ExploreSection/ExploreDetails/ExploreDetails.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import './ExploreDetails.sass';
+
 const ExploreDetails = props => (
-  <div>
-    <button onClick={props.toggleDetails}>Close</button>
-    <h2>Explore Details</h2>
+  <div id='ExploreDetails'>
+    <div>
+      <button onClick={props.toggleDetails}>Close</button>
+      <p>Description</p>
+    </div>
+
+    <div className='imageSlider'>
+      <div className='images'>
+        <p>Image 1</p>
+        <p>Image 2</p>
+        <p>Image 3</p>
+        <p>Image 4</p>
+      </div>
+
+      <div className='controls'>
+        <div className='buttons'>
+          <button>Left</button>
+          <button>Right</button>
+        </div>
+
+        <div>Indexing</div>
+      </div>
+    </div>
   </div>
 );
 
