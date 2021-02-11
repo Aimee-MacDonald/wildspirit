21a185c11f5aabb30f3619089851220aeb5845fa
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-02-11 17:09:29 +0200

    ExploreDetails image slider styling

--- src/frontend/components/LandingPage/ExploreSection/ExploreDetails/ExploreDetails.js
+++ src/frontend/components/LandingPage/ExploreSection/ExploreDetails/ExploreDetails.js
@@ -11,10 +11,10 @@ const ExploreDetails = props => (
 
     <div className='imageSlider'>
       <div className='images'>
-        <p>Image 1</p>
-        <p>Image 2</p>
-        <p>Image 3</p>
-        <p>Image 4</p>
+        <p className='image open'>Image 1</p>
+        <p className='image'>Image 2</p>
+        <p className='image'>Image 3</p>
+        <p className='image'>Image 4</p>
       </div>
 
       <div className='controls'>
