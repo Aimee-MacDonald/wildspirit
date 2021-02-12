ba91414df60cbd1c2aed9e6f781d17d4498f356b
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-02-12 16:44:36 +0200

    ExploreOption styling

--- src/frontend/components/LandingPage/ExploreSection/ExploreOptions/ExploreOptions.js
+++ src/frontend/components/LandingPage/ExploreSection/ExploreOptions/ExploreOptions.js
@@ -6,7 +6,7 @@ const ExploreOptions = props => (
   <div id='ExploreOptions'>
     {props.categories.map((category, index) => (
       <div key={`category_${index}`} className='exploreCategory' onClick={() => props.toggleDetails(index)}>
-        <div className='categoryImage'></div>
+        <div className='categoryImage' style={{backgroundImage: `url(${category.options[0].imageURL})`}}></div>
         <h2 className='categoryName'>{category.name}</h2>
       </div>
     ))}
