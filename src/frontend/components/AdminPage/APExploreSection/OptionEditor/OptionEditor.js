e5e5d794cb0de3955cab128f7fbc9875800870e4
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-08-19 11:50:54 +0200

    APExploreSection micro-interactions

--- src/frontend/components/AdminPage/APExploreSection/OptionEditor/OptionEditor.js
+++ src/frontend/components/AdminPage/APExploreSection/OptionEditor/OptionEditor.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './OptionEditor.sass'
 
@@ -7,71 +7,106 @@ const OptionEditor = ({optionDetails, setOptionDetails, categoryDetails}) => {
   const setDescription = description => setOptionDetails({...optionDetails, description})
   const setImageAlt = imageAlt => setOptionDetails({...optionDetails, imageAlt})
 
+  const postingStates = [
+    'default',
+    'posting',
+    'success',
+    'failed'
+  ]
+
+  const [ postingState, setPostingState ] = useState(0)
+
   const saveOption = e => {
     e.preventDefault()
+
+    setPostingState(postingStates.indexOf('posting'))
     
     fetch('/api/exploreOption', {
       method: 'post',
       body: new FormData(e.target)
     }).then(res => res.json())
-      .then(result => console.log(result))
-      .catch(error => console.log(error))
+      .then(result => {
+        if(result === 'Created') {
+          setPostingState(postingStates.indexOf('success'))
+        }
+      })
+      .catch(error => setPostingState(postingStates.indexOf('failed')))
   }
 
   return(
-    <form id='OptionEditor' onSubmit={saveOption}>
-      <input
-        name='categoryID'
-        value={categoryDetails._id}
-        readOnly
-        hidden
-      />
+    <div>
+      {postingState === postingStates.indexOf('default') &&
+        <form id='OptionEditor' onSubmit={saveOption}>
+          <input
+            name='categoryID'
+            value={categoryDetails._id}
+            readOnly
+            hidden
+          />
 
-      <input
-        name='categoryName'
-        value={categoryDetails.name}
-        readOnly
-        hidden
-      />
+          <input
+            name='categoryName'
+            value={categoryDetails.name}
+            readOnly
+            hidden
+          />
 
-      <input
-        name='imageID'
-        value={optionDetails.imageID}
-        readOnly
-        hidden
-      />
+          <input
+            name='imageID'
+            value={optionDetails.imageID}
+            readOnly
+            hidden
+          />
 
-      <label htmlFor='optionName'>Name</label>
-      <input
-        id='optionName'
-        name='optionName'
-        value={optionDetails.name}
-        onChange={e => setName(e.target.value)}
-      />
+          <label htmlFor='optionName'>Name</label>
+          <input
+            id='optionName'
+            name='optionName'
+            value={optionDetails.name}
+            onChange={e => setName(e.target.value)}
+          />
 
-      <label htmlFor='optionDescription'>Description</label>
-      <textarea
-        id='optionDescription'
-        name='optionDescription'
-        value={optionDetails.description}
-        onChange={e => setDescription(e.target.value)}
-      />
+          <label htmlFor='optionDescription'>Description</label>
+          <textarea
+            id='optionDescription'
+            name='optionDescription'
+            value={optionDetails.description}
+            onChange={e => setDescription(e.target.value)}
+          />
 
-      {/*
-        Image Selection
-        3 Links
-      */}
+          <input type='file' name='image'/>
+          <input
+            placeholder='ImageDescription'
+            name='imgAlt'
+            value={optionDetails.imageAlt}
+            onChange={e => setImageAlt(e.target.value)}
+          />
 
-      <input type='file' name='image'/>
-      <input
-        placeholder='ImageDescription'
-        name='imgAlt'
-        value={optionDetails.imageAlt}
-        onChange={e => setImageAlt(e.target.value)}
-      />
+          <button type='submit'>Save</button>
+        </form>
+      }
 
-      <button type='submit'>Save</button>
-    </form>
+      {postingState === postingStates.indexOf('posting') &&
+        <div className='enquiring'>
+          <div className='spinner'><div></div></div>
+          <h1>Saving</h1>
+        </div>
+      }
+
+      {postingState === postingStates.indexOf('success') &&
+        <div className='enquiring'>
+          <h1>Success</h1>
+          <button onClick={() => setPostingState(postingStates.indexOf('default'))}>Reset</button>
+        </div>
+      }
+
+      {postingState === postingStates.indexOf('failed') &&
+        <div className='enquiring'>
+          <h1>Failed</h1>
+          <button onClick={() => setPostingState(postingStates.indexOf('default'))}>Reset</button>
+        </div>
+      }
+    </div>
   )
 }
 
