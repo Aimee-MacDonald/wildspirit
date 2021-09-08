8e7d158cf66bf3a0c69b2934499621df6f900802
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-09-08 13:54:29 +0200

    Removed logo

--- src/frontend/components/LandingPage/Navigation/Navigation.js
+++ src/frontend/components/LandingPage/Navigation/Navigation.js
@@ -5,8 +5,6 @@ import './logo.svg'
 
 const Navigation = () => (
   <nav id='Navigation'>
-    <img src={'./images/logo.svg'} id='logo'/>
-
     <ul>
       <li><a href='#LiveSection'>Live</a></li>
       <li><a href='#LearnSection'>Learn</a></li>
